65ab8c8d67074ec3f1c41eebe38f70e763038ab9
yangjunsik <wnstlr1379@naver.com>
2025-05-22 15:12:11 +0900

    organization 변경 및 deploy파일 ecr 로그인 코드 수정 #1

--- src/main/java/com/convention_store/controller/AppController.java
+++ src/main/java/com/convention_store/controller/AppController.java
@@ -1,4 +1,4 @@
-package com.convention_store;
+package com.convention_store.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
