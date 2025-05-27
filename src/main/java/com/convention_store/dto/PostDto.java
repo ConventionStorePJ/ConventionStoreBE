d74166728fdc2cfb5540e492a353f0d6e8ed48f4
moozeh <kcw2205@naver.com>
2025-05-27 19:50:06 +0900

    fix: `PostDto` Getter, Builder 추가

--- src/main/java/com/convention_store/dto/PostDto.java
+++ src/main/java/com/convention_store/dto/PostDto.java
@@ -1,7 +1,11 @@
 package com.convention_store.dto;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
 public class PostDto {
     private Long postId;
     private String title;
