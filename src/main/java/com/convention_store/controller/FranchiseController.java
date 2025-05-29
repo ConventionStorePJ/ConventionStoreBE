d26943c4456c7bf3e008fe6b314f54f8888b67ee
moozeh <kcw2205@naver.com>
2025-05-29 11:14:04 +0900

    feat: Franchise 반환하는 API 추가

---
+++ src/main/java/com/convention_store/controller/FranchiseController.java
@@ -0,0 +1,23 @@
+package com.convention_store.controller;
+
+import com.convention_store.dto.FranchiseDto;
+import com.convention_store.service.FranchiseService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/franchises")
+@RequiredArgsConstructor
+public class FranchiseController {
+    
+    private final FranchiseService franchiseService;
+    
+    @GetMapping("")
+    public ResponseEntity<List<FranchiseDto>> getAllFranchises() {
+        return ResponseEntity.ok(franchiseService.getAllFranchises());
+    }
+}
--- src/main/java/com/convention_store/dto/FranchiseDto.java
+++ src/main/java/com/convention_store/dto/FranchiseDto.java
@@ -1,5 +1,6 @@
 package com.convention_store.dto;
 
+import com.convention_store.domain.Franchise;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,4 +9,8 @@ import lombok.Getter;
 public class FranchiseDto {
     private Long franchiseId;
     private String franchiseName;
+    
+    public static FranchiseDto from(Franchise franchise) {
+        return FranchiseDto.builder().franchiseId(franchise.getId()).franchiseName(franchise.getName()).build();
+    }
 }
